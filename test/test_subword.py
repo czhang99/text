e99d67f6ce3c317e2e4015630800feeee549cf15
Bryan Marcus McCann <Bryan.McCann.is@gmail.com>
2017-09-11 14:31:12 +0000

    removing snli test

--- test/test_subword.py
+++ test/test_subword.py
@@ -18,21 +18,6 @@ class TestSubword(unittest.TestCase):
         batch = next(iter(data.Iterator(cooked, 1, shuffle=False)))
         self.assertEqual(TEXT.reverse(batch.text.data)[0], raw[0].text)
 
-    def test_subword_snli(self):
-        TEXT = data.SubwordField()
-        LABEL = data.Field(sequential=False)
-        RAW = data.Field(sequential=False, use_vocab=False)
-        raw, = SNLI.splits(RAW, LABEL, train=None, test=None)
-        cooked, = SNLI.splits(TEXT, LABEL, train=None, test=None)
-        LABEL.build_vocab(cooked)
-        TEXT.build_vocab(cooked, max_size=100)
-        TEXT.segment(cooked)
-        print(cooked[0].premise)
-        print(cooked[0].hypothesis)
-        batch = next(iter(data.Iterator(cooked, 1, shuffle=False)))
-        self.assertEqual(TEXT.reverse(batch.premise.data)[0], raw[0].premise)
-        self.assertEqual(TEXT.reverse(batch.hypothesis.data)[0], raw[0].hypothesis)
-
 
 if __name__ == '__main__':
     unittest.main()
